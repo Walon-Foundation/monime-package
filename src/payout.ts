273448746b1db44042752705cf3e30fb4af41f52
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-10-05 16:02:20 +0000

    Add CreatePayout functionality and type definitions for mobile money payouts

---
+++ src/payout.ts
@@ -0,0 +1,59 @@
+import axios from "axios"
+import { randomBytes } from "crypto"
+import { CreatePayout } from "./payoutTypes"
+
+const URL = ""
+const value = randomBytes(20).toString("hex")
+
+interface Return {
+    data?:CreatePayout,
+    error?:Error,
+    success:boolean
+}
+
+export async function CreatePayoutMobileMoney(amount:number,phoneNumber:string, sourceAccount:string,  monime_access_token:string, monime_space_id:string):Promise<Return> {
+    let provider = "m17"
+    const africell = ["077", "033", "088", "080","090","030"]
+    for (let value of africell){
+        if(phoneNumber.startsWith(value)){
+            provider = 'm18'
+            break;
+        }
+    }
+    const body = {
+        amount:{
+            currency:'SLE',
+            value:amount
+        },
+        source:{
+            financialAccountId:sourceAccount
+        },
+        destination:{
+            type:"momo",
+            provider:provider,
+            phoneNumber:phoneNumber
+        },
+        metadata:{}
+    }
+
+    try{
+        const res = await axios.post(URL, body, {
+            headers:{
+                'Idempotency-Key': `${value}`,
+                'Monime-Space-Id': `${monime_space_id}`,
+                Authorization: `Bearer ${monime_access_token}`,
+                'Content-Type': 'application/json'  
+            }
+        })
+
+        const data = res.data as CreatePayout
+
+        return {success:true, data}
+    }catch(error){
+        if(axios.isAxiosError(error)){
+            return { success:false, error:error}
+        }
+
+        return { success:false, error: new Error("unknown error")}
+    }   
+}
---
+++ src/payoutTypes.ts
@@ -0,0 +1,68 @@
+export interface CreatePayout {
+  success: boolean;
+  messages: string[];
+  result: Result;
+}
+
+interface Result {
+  id: string;
+  status: string;
+  amount: Amount;
+  source: Source;
+  destination: Destination;
+  fees: Fee[];
+  failureDetail: FailureDetail;
+  createTime: string;
+  updateTime: string;
+  ownershipGraph: OwnershipGraph;
+  metadata: Metadata;
+}
+
+interface OwnershipGraph {
+  owner: Owner2;
+}
+
+interface Owner2 {
+  id: string;
+  type: string;
+  metadata: Metadata;
+  owner: Owner;
+}
+
+interface Owner {
+  id: string;
+  type: string;
+  metadata: Metadata;
+  owner: Metadata;
+}
+
+interface FailureDetail {
+  code: string;
+  message: string;
+}
+
+interface Fee {
+  code: string;
+  amount: Amount;
+  metadata: Metadata;
+}
+
+interface Metadata {
+}
+
+interface Destination {
+  type: string;
+  providerId: string;
+  accountNumber: string;
+  transactionReference: string;
+}
+
+interface Source {
+  financialAccountId: string;
+  transactionReference: string;
+}
+
+interface Amount {
+  currency: string;
+  value: number;
+}
