8aa729ec3bd3b96e404f79d47f69c3fca0b608cd
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-10-11 14:12:50 +0000

    update the client and fix the main index.ts

--- src/client.ts
+++ src/client.ts
@@ -1,25 +1,8 @@
-import {
-	createFinancialAccount,
-	getFinancialAccount,
-	getAllFinancialAccount
-} from "./modules/financialAccount/financialAccount";
-import { createInternalTransfer,getAllInternalTransfers, getInternalTransfer, deleteInternalTransfer } from "./modules/internalTransfer/internalTransfer";
-import {
-	createPaymentCode,
-	deletePaymentCode,
-} from "./modules/paymentCode/paymentCode";
-import { CreatePayoutMobileMoney } from "./modules/payout/payout";
-import type {
-	AllFinancialAccount,
-	AllInternalTransfers,
-	CreateFinancialAccount,
-	CreateInternalTransfer,
-	CreatePaymentCode,
-	CreatePayout,
-	GetFinancialAccount,
-	InternalTransfer,
-	DeleteTransfer
-} from "./modules/types";
+import { FinancialAccountAPI } from "./modules/financialAccount";
+import { FinancialTransactionAPI } from "./modules/financialTransaction";
+import { InternalTransferAPI } from "./modules/internalTransfer";
+import { PaymentCodeAPI } from "./modules/paymentCode";
+import { PayoutAPI } from "./modules/payout";
 
 export interface ClientOptions {
 	monimeSpaceeId: string;
@@ -30,99 +13,21 @@ export class MonimeClient {
 	private monimeSpaceId: string;
 	private accessToken: string;
 
-	/* Methods */
-
-	//method for financial accounts
-	createFinancialAccount: (name: string) => Promise<{
-		success: boolean;
-		data?: CreateFinancialAccount;
-		error?: Error;
-	}>;
-	getFinancialAccount: (
-		financialAccountId: string,
-	) => Promise<{ success: boolean; data?: GetFinancialAccount; error?: Error }>;
-
-	getAllFinancialAccount:(
-	) => Promise<{ success:boolean, data?:AllFinancialAccount, error?:Error}>
-
-	//method for internal transfer
-	createInternalTransfer: (
-		sourceAccount: string,
-		destinationAccount: string,
-		amount: number,
-	) => Promise<{
-		success: boolean;
-		data?: CreateInternalTransfer;
-		error?: Error;
-	}>;
-	getInternalTransfer: (
-		internalTransferId:string
-	) => Promise<{ success:boolean, data?:InternalTransfer, error?:Error}>;
-	getAllInternalTransfer: (
-	) => Promise<{ success:boolean, data?:AllInternalTransfers, error?:Error}>;
-	deleteInternalTransfer:(
-		internalTransferId:string
-	) => Promise<{ success:boolean, data?:DeleteTransfer, error?:Error}>;
-
-	//method for payment code
-	createPaymentCode: (
-		paymentName: string,
-		amount: number,
-		financialAccount: string,
-		username: string,
-		phoneNumber: string,
-	) => Promise<{ success: boolean; error?: Error; data?: CreatePaymentCode }>;
-	deletePaymentCode: (
-		paymentCodeId: string,
-	) => Promise<{ success: boolean; error?: Error; data?: CreatePaymentCode }>;
-
-	//method for payout
-	createPayout: (
-		amount: number,
-		phoneNumber: string,
-		sourceAccount: string,
-	) => Promise<{ success: boolean; error?: Error; data?: CreatePayout }>;
+	public financialAccount: ReturnType<typeof FinancialAccountAPI>;
+	public internalTransfer: ReturnType<typeof InternalTransferAPI>;
+	public paymentCode: ReturnType<typeof PaymentCodeAPI>;
+	public payout: ReturnType<typeof PayoutAPI>;
+	public financialTransaction: ReturnType<typeof FinancialTransactionAPI>
 
 	constructor(options: ClientOptions) {
 		this.accessToken = options.accessToken;
 		this.monimeSpaceId = options.monimeSpaceeId;
 
-		this.createFinancialAccount = (name: string) =>
-			createFinancialAccount(name, this);
-		this.getFinancialAccount = (financialAccountId: string) =>
-			getFinancialAccount(financialAccountId, this);
-		this.createInternalTransfer = (
-			sourceAccount: string,
-			destinationAccount: string,
-			amount: number,
-		) =>
-			createInternalTransfer(sourceAccount, destinationAccount, this, amount);
-		this.createPaymentCode = (
-			paymentName: string,
-			amount: number,
-			financialAccount: string,
-			username: string,
-			phoneNumber: string,
-		) =>
-			createPaymentCode(
-				paymentName,
-				amount,
-				financialAccount,
-				username,
-				phoneNumber,
-				this,
-			);
-		this.deletePaymentCode = (paymentCodeId: string) =>
-			deletePaymentCode(paymentCodeId, this);
-		this.createPayout = (
-			amount: number,
-			phoneNumber: string,
-			sourceAccount: string,
-		) => CreatePayoutMobileMoney(amount, phoneNumber, sourceAccount, this);
-		this.getAllFinancialAccount = () => getAllFinancialAccount(this)
-		this.getAllInternalTransfer = () => getAllInternalTransfers(this);
-		this.getInternalTransfer = (internalFinancialId) => getInternalTransfer(this, internalFinancialId);
-		this.deleteInternalTransfer = (internalFinancialId) => deleteInternalTransfer(this, internalFinancialId)
+		this.financialAccount = FinancialAccountAPI(this);
+		this.internalTransfer = InternalTransferAPI(this);
+		this.paymentCode = PaymentCodeAPI(this);
+		this.payout = PayoutAPI(this);
+		this.financialTransaction = FinancialTransactionAPI(this)
 	}
 
 	/** @internal */
